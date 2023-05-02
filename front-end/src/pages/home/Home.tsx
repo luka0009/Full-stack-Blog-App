c2de88a1c7e7a3897262525bfe3abfd16738c817
luka <lukabarbakadze18@gmail.com>
2023-05-02 23:01:27 +0400

    cta section

--- front-end/src/pages/home/Home.tsx
+++ front-end/src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import MainLayout from "../../components/MainLayout"
 import Articles from "./container/Articles"
+import CTA from "./container/CTA"
 import Hero from "./container/Hero"
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <MainLayout>
       <Hero />
       <Articles />
+      <CTA />
     </MainLayout>
   )
 }
--- front-end/src/pages/home/container/Articles.tsx
+++ front-end/src/pages/home/container/Articles.tsx
@@ -1,11 +1,18 @@
 import ArticleCard from "../../../components/ArticleCard"
+import { FaArrowRight } from 'react-icons/fa';
 
 const Articles = () => {
   return (
-    <section className="container mx-auto grid grid-cols-1 gap-y-12 xs:grid-cols-2 gap-x-12 lg:grid-cols-3 lg:gap-x-6 px-0 py-6 md:py-0 lg:-mt-6">
+    <section className="flex flex-col container mx-auto px-0 py-6 md:py-0 lg:-mt-6">
+      <div className="pb-10 grid grid-cols-1 gap-y-12 xs:grid-cols-2 gap-x-12 lg:grid-cols-3 lg:gap-x-6">
       <ArticleCard className="w-80 mx-auto xs:w-auto sm:w-full"/>
       <ArticleCard className="w-80 mx-auto xs:w-auto sm:w-full"/>
       <ArticleCard className="w-80 mx-auto xs:w-auto sm:w-full"/>
+      </div>
+      <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
+        <span>More articles</span>
+        <FaArrowRight className="w-3 h-3" />
+      </button>
     </section>
   )
 }
