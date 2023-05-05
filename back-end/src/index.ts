3617c58f469a66591750abb59e5b52fa605429ee
luka <lukabarbakadze18@gmail.com>
2023-05-05 20:15:20 +0400

    backend setup with typescript

---
+++ back-end/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app: express.Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on Port:', PORT);
+})
