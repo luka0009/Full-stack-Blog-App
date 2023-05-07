281519a41ccf87bd8eaa19fffaa7c81c5f9b7dc9
luka <lukabarbakadze18@gmail.com>
2023-05-07 22:35:50 +0400

    redux setup

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { store } from './store/store.ts';
+import { Provider } from 'react-redux'
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
