ce71a7044a0d2452d3012bbd23d5bbaab0d4016e
luka <lukabarbakadze18@gmail.com>
2023-05-09 21:33:41 +0400

    deleting profile pic

--- front-end/src/components/ProfilePic.tsx
+++ front-end/src/components/ProfilePic.tsx
@@ -4,14 +4,42 @@ import { createPortal } from "react-dom";
 import { HiOutlineCamera } from "react-icons/hi";
 import EasyCrop from "./crop/EasyCrop";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateProfilePicture } from "../services/users";
+import { setUserInfo } from "../store/features/user/userSlice";
 
 type Props = {
   avatar: string;
 };
 
 const ProfilePic = ({ avatar }: Props) => {
+  const queryClient = useQueryClient();
+  const dispatch = useAppDispatch();
+  const userInfo = useAppSelector((state) => state.user.userInfo);
   const [openCrop, setOpenCrop] = useState(false);
   const [photo, setPhoto] = useState(null);
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: ({ token, formData }: any) => {
+      return updateProfilePicture({
+        token: token,
+        formData: formData,
+      });
+    },
+    onSuccess: (data) => {
+      dispatch(setUserInfo(data));
+      setOpenCrop(false);
+      localStorage.setItem("user", JSON.stringify(data));
+      queryClient.invalidateQueries(["profile"]);
+      toast.success("Profile Photo is removed");
+    },
+    onError: (error: any) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files[0];
@@ -19,14 +47,25 @@ const ProfilePic = ({ avatar }: Props) => {
     setOpenCrop(true);
   };
 
+  const handleImageDelete = () => {
+    if (window.confirm("Do you want to delete your picture")) {
+      try {
+        const formData = new FormData();
+        formData.append("profilePicture", undefined);
+
+        mutate({ token: userInfo.token, formData: formData });
+      } catch (error) {
+        toast.error(error.message);
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <>
       {openCrop &&
         createPortal(
-          <EasyCrop 
-          photo={photo}
-          setOpenCrop={setOpenCrop}
-          />,
+          <EasyCrop photo={photo} setOpenCrop={setOpenCrop} />,
           document.getElementById("portal")
         )}
       <div className="w-full flex items-center gap-x-4 mb-5">
@@ -55,7 +94,7 @@ const ProfilePic = ({ avatar }: Props) => {
           />
         </div>
         <button
-          onClick={() => console.log("aslkdj")}
+          onClick={handleImageDelete}
           type="button"
           className="border border-red-500 rounded-lg px-4 py-2 text-red-500"
         >
