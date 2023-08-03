52ec3b01bd98e2e27bb42c43f7d18ae0bd49e9eb
luka <lukabarbakadze18@gmail.com>
2023-08-03 11:06:41 +0400

    profile page bug fixed

--- front-end/src/pages/profile/ProfilePage.tsx
+++ front-end/src/pages/profile/ProfilePage.tsx
@@ -7,177 +7,179 @@ import { updateProfile, getUserProfile } from "../../services/users";
 import toast from "react-hot-toast";
 import { useAppSelector, useAppDispatch } from "../../store/hooks";
 import { setUserInfo } from "../../store/features/user/userSlice";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import ProfilePic from "../../components/ProfilePic";
 
 const ProfilePage = () => {
-  const userInfo = useAppSelector((state) => state.user.userInfo);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const queryClient = useQueryClient();
+	const userInfo = useAppSelector((state) => state.user.userInfo);
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
+	const queryClient = useQueryClient();
 
-  const {
-    data: profileData,
-    isLoading: profileIsLoading,
-    error: profileError,
-  } = useQuery({
-    queryFn: () => {
-      return getUserProfile({ token: userInfo.token });
-    },
-    queryKey: ["profile"],
-  });
+	const {
+		data: profileData,
+		isLoading: profileIsLoading,
+		error: profileError,
+	} = useQuery({
+		queryFn: () => {
+			return getUserProfile({ token: userInfo.token });
+		},
+		queryKey: ["profile"],
+	});
 
-  const { mutate, isLoading: updateProfileIsLoading } = useMutation({
-    mutationFn: ({ name, email, password }) => {
-      return updateProfile({
-        token: userInfo.token,
-        userData: { name, email, password },
-      });
-    },
-    onSuccess: (data) => {
-      dispatch(setUserInfo(data));
-      localStorage.setItem("user", JSON.stringify(data));
-      queryClient.invalidateQueries(["profile"]);
-      toast.success("Profile is updated");
-    },
-    onError: (error) => {
-      toast.error(error.message);
-      console.log(error);
-    },
-  });
+	const { mutate, isLoading: updateProfileIsLoading } = useMutation({
+		mutationFn: ({ name, email, password }) => {
+			return updateProfile({
+				token: userInfo.token,
+				userData: { name, email, password },
+			});
+		},
+		onSuccess: (data) => {
+			dispatch(setUserInfo(data));
+			localStorage.setItem("user", JSON.stringify(data));
+			queryClient.invalidateQueries(["profile"]);
+			toast.success("Profile is updated");
+		},
+		onError: (error) => {
+			toast.error(error.message);
+			console.log(error);
+		},
+	});
 
-  useEffect(() => {
-    if (!userInfo) {
-      navigate("/");
-    }
-  }, [navigate, userInfo]);
+	useEffect(() => {
+		if (!userInfo) {
+			navigate("/");
+		}
+	}, [navigate, userInfo]);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({
-    defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-    },
-    values: {
-      name: profileIsLoading ? "" : profileData.name,
-      email: profileIsLoading ? "" : profileData.email,
-    },
-    mode: "onChange",
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm({
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+		},
+		values: useMemo(() => {
+			return {
+				name: profileIsLoading ? "" : profileData.name,
+				email: profileIsLoading ? "" : profileData.email,
+			};
+		}, [profileData?.name, profileData?.email]),
+		mode: "onChange",
+	});
 
-  const submitHandler = (data) => {
-    const { name, email, password } = data;
-    mutate({ name, email, password });
-  };
+	const submitHandler = (data) => {
+		const { name, email, password } = data;
+		mutate({ name, email, password });
+	};
 
-  return (
-    <MainLayout>
-      <section className="container mx-auto px-5 py-10">
-        <div className="w-full max-w-sm mx-auto">
-          <ProfilePic avatar={profileData?.avatar} />
-          <form onSubmit={handleSubmit(submitHandler)}>
-            <div className="flex flex-col mb-6 w-full">
-              <label
-                htmlFor="name"
-                className="text-[#5a7184] font-semibold block"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                {...register("name", {
-                  minLength: {
-                    value: 1,
-                    message: "Name length must be at least 1 character",
-                  },
-                  required: {
-                    value: true,
-                    message: "Name is required",
-                  },
-                })}
-                placeholder="Enter name"
-                className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
-                  errors.name ? "border-red-500" : "border-[#c3cad9]"
-                }`}
-              />
-              {errors.name?.message && (
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.name?.message}
-                </p>
-              )}
-            </div>
-            <div className="flex flex-col mb-6 w-full">
-              <label
-                htmlFor="email"
-                className="text-[#5a7184] font-semibold block"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                {...register("email", {
-                  pattern: {
-                    value:
-                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: "Enter a valid email",
-                  },
-                  required: {
-                    value: true,
-                    message: "Email is required",
-                  },
-                })}
-                placeholder="Enter email"
-                className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
-                  errors.email ? "border-red-500" : "border-[#c3cad9]"
-                }`}
-              />
-              {errors.email?.message && (
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.email?.message}
-                </p>
-              )}
-            </div>
-            <div className="flex flex-col mb-6 w-full">
-              <label
-                htmlFor="password"
-                className="text-[#5a7184] font-semibold block"
-              >
-                New Password (optional)
-              </label>
-              <input
-                type="password"
-                id="password"
-                {...register("password")}
-                placeholder="Enter new password"
-                className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
-                  errors.password ? "border-red-500" : "border-[#c3cad9]"
-                }`}
-              />
-              {errors.password?.message && (
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.password?.message}
-                </p>
-              )}
-            </div>
-            <button
-              type="submit"
-              disabled={!isValid || profileIsLoading || updateProfileIsLoading}
-              className="bg-primary text-white font-bold text-lg py-3 px-2 w-full rounded-lg mb-6 disabled:opacity-70 disabled:cursor-not-allowed"
-            >
-              Update
-            </button>
-          </form>
-        </div>
-      </section>
-    </MainLayout>
-  );
+	return (
+		<MainLayout>
+			<section className="container mx-auto px-5 py-10">
+				<div className="w-full max-w-sm mx-auto">
+					<ProfilePic avatar={profileData?.avatar} />
+					<form onSubmit={handleSubmit(submitHandler)}>
+						<div className="flex flex-col mb-6 w-full">
+							<label
+								htmlFor="name"
+								className="text-[#5a7184] font-semibold block"
+							>
+								Name
+							</label>
+							<input
+								type="text"
+								id="name"
+								{...register("name", {
+									minLength: {
+										value: 1,
+										message: "Name length must be at least 1 character",
+									},
+									required: {
+										value: true,
+										message: "Name is required",
+									},
+								})}
+								placeholder="Enter name"
+								className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
+									errors.name ? "border-red-500" : "border-[#c3cad9]"
+								}`}
+							/>
+							{errors.name?.message && (
+								<p className="text-red-500 text-xs mt-1">
+									{errors.name?.message}
+								</p>
+							)}
+						</div>
+						<div className="flex flex-col mb-6 w-full">
+							<label
+								htmlFor="email"
+								className="text-[#5a7184] font-semibold block"
+							>
+								Email
+							</label>
+							<input
+								type="email"
+								id="email"
+								{...register("email", {
+									pattern: {
+										value:
+											/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+										message: "Enter a valid email",
+									},
+									required: {
+										value: true,
+										message: "Email is required",
+									},
+								})}
+								placeholder="Enter email"
+								className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
+									errors.email ? "border-red-500" : "border-[#c3cad9]"
+								}`}
+							/>
+							{errors.email?.message && (
+								<p className="text-red-500 text-xs mt-1">
+									{errors.email?.message}
+								</p>
+							)}
+						</div>
+						<div className="flex flex-col mb-6 w-full">
+							<label
+								htmlFor="password"
+								className="text-[#5a7184] font-semibold block"
+							>
+								New Password (optional)
+							</label>
+							<input
+								type="password"
+								id="password"
+								{...register("password")}
+								placeholder="Enter new password"
+								className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
+									errors.password ? "border-red-500" : "border-[#c3cad9]"
+								}`}
+							/>
+							{errors.password?.message && (
+								<p className="text-red-500 text-xs mt-1">
+									{errors.password?.message}
+								</p>
+							)}
+						</div>
+						<button
+							type="submit"
+							disabled={!isValid || profileIsLoading || updateProfileIsLoading}
+							className="bg-primary text-white font-bold text-lg py-3 px-2 w-full rounded-lg mb-6 disabled:opacity-70 disabled:cursor-not-allowed"
+						>
+							Update
+						</button>
+					</form>
+				</div>
+			</section>
+		</MainLayout>
+	);
 };
 
 export default ProfilePage;
